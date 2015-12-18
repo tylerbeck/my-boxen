148d56f23c8d55758ffc7e7e692d45560c12bf6e
Tyler Beck <tyler@tylerbeck.com>
2015-12-18 14:49:46 -0500

    merging with original

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,7 @@
 require boxen::environment
 require homebrew
 require gcc
+require git
 
 Exec {
   group       => 'staff',
@@ -52,36 +53,11 @@ Service {
 Homebrew::Formula <| |> -> Package <| |>
 
 node default {
-  # core modules, needed for most things
-  include dnsmasq
-  include git
-  include hub
-  include nginx
-
   # fail if FDE is not enabled
   if $::root_encrypted == 'no' {
     fail('Please enable full disk encryption and try again')
   }
 
-  # node versions
-  nodejs::version { '0.8': }
-  nodejs::version { '0.10': }
-  nodejs::version { '0.12': }
-
-  # default ruby versions
-  ruby::version { '1.9.3': }
-  ruby::version { '2.0.0': }
-  ruby::version { '2.1.7': }
-  ruby::version { '2.2.3': }
-
-  # common, useful packages
-  package {
-    [
-      'ack',
-      'findutils',
-      'gnu-tar'
-    ]:
-  }
 
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
